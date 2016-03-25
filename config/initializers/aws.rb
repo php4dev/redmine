7cbd567ca7052dc6db3de9c5953b43584f2363ae
kalbunna <kal.bunna@gmail.com>
2016-03-25 10:13:49 +0700

    add aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,3 +3,9 @@
 #     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
 #     region: ENV['AWS_REGION']
 # )
+#
+AWS.config({
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: ENV['AWS_REGION']
+})
